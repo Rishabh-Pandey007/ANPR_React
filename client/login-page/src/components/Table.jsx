b89111d29f9855a8af8494464051afd0f7c98fd7
Ruban <rubannadar20@gmail.com>
2023-12-04 13:49:33 +0530

    logs

--- client/login-page/src/components/Table.jsx
+++ client/login-page/src/components/Table.jsx
@@ -1,19 +1,36 @@
 import React, { useState, useEffect } from "react";
 import '../styles/Table.css';
-// import axios from "axios";
+import axios from "axios";
+
+
+// fetching data from api
+// const Data = () => {
+
+//   const fetchData = async () => {
+//       try {
+//           const response = axios.get('http://127.0.0.1:5001/v1/logs', {});
+
+//       } catch (error) {
+//           // Handle errors, show an error message, etc.
+//           console.error('Error during login:', error);
+//       }
+//   };
 
 const TableDisplay = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     // For demonstration purposes, let's assume the API response is an object
-    let response = {
-      id: 1,
-      image: "image1.jpg",
-      vehicleno: "TN 01 AB 1234",
-      date: "2021-01-01",
-      time: "10:00:00",
-    };
+    // let response = {
+    //   id: 1,
+    //   image: "image1.jpg",
+    //   vehicleno: "TN 01 AB 1234",
+    //   date: "2021-01-01",
+    //   time: "10:00:00",
+    // };
+
+    const response = axios.get('http://127.0.0.1:5001/v1/logs', {});
+
 
     // Wrap the response in an array if it's an object
     setData(Array.isArray(response) ? response : [response]);
@@ -47,7 +64,7 @@ const TableDisplay = () => {
               <tr key={item.id}>
                 <td>{item.id}</td>
                 <td>{item.image}</td>
-                <td>{item.vehicleno}</td>
+                <td>{item.vehicleRegistration}</td>
                 <td>{item.date}</td>
                 <td>{item.time}</td>
               </tr>
