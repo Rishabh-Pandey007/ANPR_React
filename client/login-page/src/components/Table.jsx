f7936b41c46254b75d4dcabc1d54b99521a04be7
Rishabh-Pandey007 <rishabhpandey2023@gmail.com>
2023-12-04 14:55:54 +0530

    integrated login

--- client/login-page/src/components/Table.jsx
+++ client/login-page/src/components/Table.jsx
@@ -2,71 +2,55 @@ import React, { useState, useEffect } from "react";
 import '../styles/Table.css';
 import axios from "axios";
 
-
-// fetching data from api
-// const Data = () => {
-
-//   const fetchData = async () => {
-//       try {
-//           const response = axios.get('http://127.0.0.1:5001/v1/logs', {});
-
-//       } catch (error) {
-//           // Handle errors, show an error message, etc.
-//           console.error('Error during login:', error);
-//       }
-//   };
-
 const TableDisplay = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    // For demonstration purposes, let's assume the API response is an object
-    // let response = {
-    //   id: 1,
-    //   image: "image1.jpg",
-    //   vehicleno: "TN 01 AB 1234",
-    //   date: "2021-01-01",
-    //   time: "10:00:00",
-    // };
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:5001/v1/logs', {});
+        setData(response.data); // Assuming the data is in the 'data' property
+      } catch (error) {
+        console.error('Error during API request:', error);
+      }
+    };
 
-    const response = axios.get('http://127.0.0.1:5001/v1/logs', {});
-
-
-    // Wrap the response in an array if it's an object
-    setData(Array.isArray(response) ? response : [response]);
+    fetchData();
   }, []);
 
   return (
     <div className="containers">
-        <div className="headers">
-            <div className="text">Vehicle Entry</div>
-            <div className="underlines"></div>
-        </div><br></br>
+      <div className="headers">
+        <div className="text">Vehicle Entry</div>
+        <div className="underlines"></div>
+      </div><br></br>
       <table>
         <thead>
           <tr>
             <th>ID</th>
-            <th>Image</th>
             <th>Vehicle No</th>
             <th>Event-Date</th>
             <th>Event-Time</th>
+            <th>Image</th>
+            <th>Spot</th>
           </tr>
         </thead>
         <tbody>
-          {!data.length ? (
+          {!data.rows || !data.rows.length ? (
             <tr>
               <td colSpan="5" className="text-center">
                 No data found
               </td>
             </tr>
           ) : (
-            data.map((item) => (
+            data.rows.map((item) => (
               <tr key={item.id}>
                 <td>{item.id}</td>
-                <td>{item.image}</td>
                 <td>{item.vehicleRegistration}</td>
-                <td>{item.date}</td>
-                <td>{item.time}</td>
+                <td>{item.redableEventTimeStamp.date}</td> {/* Accessing date property */}
+                <td>{item.redableEventTimeStamp.time}</td> {/* Accessing time property */}
+                <td>{item.vehicleImage}</td> {/* Corrected property name */}
+                <td>TIMSCDR</td>
               </tr>
             ))
           )}
